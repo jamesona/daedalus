82c8e493d4cab3328192a074ea08dd4b99ad97f6
Jameson Aranda <jamesonaranda@gmail.com>
2017-07-23 10:14:08 -0600

    added syncronous listener handling

--- game/js/events.js
+++ game/js/events.js
@@ -1,24 +1,25 @@
 'use strict'
 
 class EventEmitter {
-	constructor(after) {
+	constructor(options) {
 		this.events = {}
-		this.after = after
+		this.after = options.after || () => {}
+		this.async = options.async || true
 	}
 
-	on(event, listener) {
+	on(event, listener, index) {
 		if (typeof this.events[event] !== 'object') {
 	        this.events[event] = []
 	    }
-
-    	this.events[event].push(listener)
+		if (index)
+			this.events[event].splice(index, 0, listener)
+		else
+    		this.events[event].push(listener)
 	}
 
 	removeListener(event, listener) {
-	    let index
-
 	    if (typeof this.events[event] === 'object') {
-	        index = this.events[event].indexOf(listener)
+			let index = this.events[event].indexOf(listener)
 
 	        if (index > -1) {
 	            this.events[event].splice(index, 1)
@@ -27,20 +28,18 @@ class EventEmitter {
 	}
 
 	emit(event) {
-	    let i
-		let listeners
-		let length
 		let args = [].slice.call(arguments, 1)
+		if (typeof this.events[event] === 'object') {
+			let listeners = this.events[event].slice()
 
-	    if (typeof this.events[event] === 'object') {
-	        listeners = this.events[event].slice()
-	        length = listeners.length
+			if (!this.async)
+				return this.cascade(args, listeners, this.after)
 
-	        for (i = 0; i < length; i++) {
-	            listeners[i].apply(this, args)
-	        }
-	    }
-		if (typeof this.after === 'function') this.after()
+			for (let i = 0; i < listeners.length; i++) {
+				listeners[i].apply(this, args)
+			}
+			this.after()
+		}
 	}
 
 	once(event, listener) {
@@ -49,5 +48,13 @@ class EventEmitter {
 	        listener.apply(this, arguments)
 	    }
 	    this.on(event, f)
+	}
+
+	cascade(args, listeners, callback) {
+		let listener = listeners.shift()
+		if (listeners.length > 0)
+			listener(args, () => { this.cascade(args, listeners, callback) })
+		else
+			listener(args, callback)
 	}
 }
