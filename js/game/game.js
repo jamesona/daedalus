ac3624760d4151d04b4be49a6d843c5a74db9007
Jameson Aranda <github@jamesonaranda.com>
2017-08-10 22:30:35 -0600

    implemented stat calculations for creatures

--- js/game/game.js
+++ js/game/game.js
@@ -1,10 +1,13 @@
 'use strict';
-module('game/world', (World) => {
+module('game/world', 'game/player', (World, Player) => {
 	class Game {
 		constructor(options) {
 			this.world = new World({
 				roomSize: options.roomSize || null
 			})
+			this.actors = [new Player()]
+			this.player = this.actors[0]
+			debugger
 		}
 	}
 
